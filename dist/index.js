c392f1096e17cae9bf57719b13437dee2b30ff18
Christian Banos <christiandepaulbp.@gmail.com>
2024-01-18 12:39:14 +0000

    making btn of the side

--- dist/index.js
+++ dist/index.js
@@ -7,28 +7,65 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const urlRickAndMOrti = 'https://rickandmortyapi.com/api/episode';
+const urlRickAndMOrti = "https://rickandmortyapi.com/api/episode";
 function getEpisodes() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const infoApi = yield fetch(urlRickAndMOrti);
             const data = yield infoApi.json();
             const episodes = data.results;
-            episodes.forEach(episode => {
-                const container = document.querySelector('#nameEpisodes');
-                container.classList.add('nameEpisodes');
-                const btnEpisode = document.createElement('button');
-                btnEpisode.classList.add('btnEpisode');
+            episodes.forEach((episode) => {
+                const container = document.querySelector("#nameEpisodes");
+                container.classList.add("nameEpisodes");
+                const btnEpisode = document.createElement("button");
+                btnEpisode.classList.add("btnEpisode");
                 btnEpisode.textContent = episode.name;
                 container === null || container === void 0 ? void 0 : container.appendChild(btnEpisode);
             });
+            return data;
         }
         catch (error) {
-            console.error('Error fetching data: ', error);
+            console.error("Error fetching data: ", error);
             throw Error;
         }
     });
 }
-getEpisodes();
+getEpisodes().then((res) => {
+    getMoreEpisodes(res);
+});
+function getMoreEpisodes(res) {
+    const btnMore = document.querySelector("#btnMore");
+    let checkEvent = true;
+    btnMore.addEventListener("click", () => __awaiter(this, void 0, void 0, function* () {
+        if (checkEvent) {
+            checkEvent = false;
+            printNewEpisodes(res);
+        }
+        else {
+        }
+    }));
+}
+function printNewEpisodes(res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            if (res.info.next) {
+                const response = yield fetch(res.info.next);
+                const data = yield response.json();
+                const NewEpisodes = data.results;
+                NewEpisodes.forEach((episode) => {
+                    const container = document.querySelector("#nameEpisodes");
+                    container.classList.add("nameEpisodes");
+                    const btnEpisode = document.createElement("button");
+                    btnEpisode.classList.add("btnEpisode");
+                    btnEpisode.textContent = episode.name;
+                    container.appendChild(btnEpisode);
+                });
+            }
+        }
+        catch (error) {
+            throw new Error("Error");
+        }
+    });
+}
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,69 @@
 import { Main, Info, Episode } from "./interface/interfaces";
 
-const urlRickAndMOrti = 'https://rickandmortyapi.com/api/episode';
+const urlRickAndMOrti = "https://rickandmortyapi.com/api/episode";
 
-async function getEpisodes() {
-    try {
-        const infoApi = await fetch(urlRickAndMOrti);
-        const data: Main = await infoApi.json();
-        const episodes: Episode[] = data.results;
-        
-        episodes.forEach(episode => {
-            const container = document.querySelector('#nameEpisodes') as HTMLButtonElement;
-            container.classList.add('nameEpisodes');
-            const btnEpisode = document.createElement('button');
-            btnEpisode.classList.add('btnEpisode')
-            btnEpisode.textContent = episode.name;
-            container?.appendChild(btnEpisode)
-            
-        })
+async function getEpisodes(): Promise<Main> {
+  try {
+    const infoApi = await fetch(urlRickAndMOrti);
+    const data: Main = await infoApi.json();
+    const episodes: Episode[] = data.results;
 
-    } catch (error) {
-        console.error('Error fetching data: ', error);
-        throw Error;
-    }
+    episodes.forEach((episode) => {
+      const container = document.querySelector(
+        "#nameEpisodes"
+      ) as HTMLButtonElement;
+      container.classList.add("nameEpisodes");
+      const btnEpisode = document.createElement("button");
+      btnEpisode.classList.add("btnEpisode");
+      btnEpisode.textContent = episode.name;
+      container?.appendChild(btnEpisode);
+    });
+    return data;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    throw Error;
+  }
 }
-getEpisodes()
+getEpisodes().then((res) => {
+  //return of the last function
+
+  // newsEpisodes.forEach((resEpisode) => {
+
+  // })
+
+  getMoreEpisodes(res);
+});
+//btn
+
+function getMoreEpisodes(res: Main): void {
+  const btnMore = document.querySelector("#btnMore") as HTMLButtonElement;
+  let checkEvent: Boolean = true;
+  btnMore.addEventListener("click", async () => {
+    if (checkEvent) {
+      checkEvent = false;
+      printNewEpisodes(res);
+    } else {
+    }
+  });
+}
+
+async function printNewEpisodes(res: Main) {
+  try {
+    if (res.info.next) {
+        const response = await fetch(res.info.next);
+        const data: Main = await response.json();
+        const NewEpisodes: Episode[] = data.results;
+    
+        NewEpisodes.forEach((episode) => {
+          const container = document.querySelector("#nameEpisodes") as HTMLButtonElement;
+          container.classList.add("nameEpisodes");
+          const btnEpisode = document.createElement("button");
+          btnEpisode.classList.add("btnEpisode");
+          btnEpisode.textContent = episode.name;
+          container.appendChild(btnEpisode);
+        });
+    }
+  } catch (error) {
+    throw new Error("Error");
+  }
+}
